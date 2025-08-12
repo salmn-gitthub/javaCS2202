d71da6cc6f57e9f6eecbed6e0d14a0e8c24bcd77
salm-gitthub <83271133+salmn-gitthub@users.noreply.github.com>
2025-08-12 11:04:22 +0530

    Update cms.java

--- cms.java
+++ cms.java
@@ -1,97 +1,88 @@
 import java.util.*;
-	
-class cms {
-	
-	public static class info {
-		String name;
-		String num;
-		
-		void update_info(String name, String num) {
-			this.name = name;
-			this.num = num;		
-		}		
-		
-	}
-		
-	public static void main (String[] args) {
-		ArrayList<info>cms = new ArrayList<info>();
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Choose any options (1-6):\n 1) Add New Contacts\n 2) Search For Contact\n 3) Update Existing Contacts\n 4) Delete Contacts\n 5) Print All Contacts\n 6) Exit");
-		int opt = sc.nextInt();
-		
-		String name;
-		String num;
-		
-		int size = 0;		
-		boolean bool = true;
-		while (bool) {
-			switch (opt) {
-			case 1:
-				System.out.println("Enter the no.of contacts: ");
-				int limit = sc.nextInt();
-				for (int i = 0; i < limit; i++) {
-					cms.add(new info());
-					System.out.println("Enter the name of contact: ");
-					name = sc.nextLine();
-					sc.nextLine();
-					System.out.println("Enter the contact number: ");
-					num = sc.nextLine();
-					cms.get(i).update_info(name, num);
-					size++;
-				}
-				break;
-			case 2:
-				System.out.println("Enter the contact to search for: ");
-				name = sc.nextLine();
-				for (int i = 0; i < size; i++) {
-					if (equalsIgnoreCase(name, cms.get(i).name)) {
-						System.out.println("Contact Found: " + cms.get(i).num);
-					}
-					else {
-						System.out.println("Contact Not Found");
-					}
-				}
-			break;
-			case 3:	
-				System.out.println("Enter the contact to update: ");
-				name = sc.nextLine();
-				System.out.println("Enter updated details:\n 1) Name:");
-				String name_new = sc.nextLine();
-				System.out.println("2) Number: ");
-				num = sc.nextLine();
-				for (int i = 0; i < size; i++) {
-					if (equalsIgnoreCase(name, cms.get(i).name)) {
-						System.out.println("Contact Before Changes:\n 1) Name: " + cms.get(i).name + "\n 2) Number: " + cms.get(i).num);
-						cms.get(i).update_info(name, num);
-						System.out.println("Contact After Changes:\n 1) Name: " + cms.get(i).name + "\n 2) Number: " + cms.get(i).num);}
-					else {
-						System.out.println("Contact Not Found");
-					}
-				}			
-			break;
-			case 4:
-				System.out.println("Enter the contact to delete: ");
-				name = sc.nextLine();
-				for (int i = 0; i < size; i++) {
-					if (equalsIgnoreCase(name, cms.get(i).name)) {
-						cms.remove(i);
-						System.out.println("Contact Deleted Successfully");
-					}
-					else {
-						System.out.println("Contact Not Found");
-					}
-				}
-			break;
-			case 5:
-				for (int i = 0; i < size; i++) {
-					System.out.println("Name: " + cms.get(i).name + "\nNumber: " + cms.get(i).num + "\n");
-				}
-			break;
-			case 6:
-			bool = false;
-			break;
-			}
-		}
-	}
+
+class Cms {
+    public static class Info {
+        String name;
+        String num;
+
+        void updateInfo(String name, String num) {
+            this.name = name;
+            this.num = num;
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Info> cms = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        boolean bool = true;
+        while (bool) {
+            System.out.println("Choose any options (1-6):\n 1) Add New Contacts\n 2) Search For Contact\n 3) Update Existing Contacts\n 4) Delete Contacts\n 5) Print All Contacts\n 6) Exit");
+            int opt = sc.nextInt();
+            sc.nextLine(); // Consume newline character
+            switch (opt) {
+                case 1:
+                    System.out.println("Enter the no.of contacts: ");
+                    int limit = sc.nextInt();
+                    sc.nextLine(); // Consume newline character
+                    for (int i = 0; i < limit; i++) {
+                        Info info = new Info();
+                        System.out.println("Enter the name of contact: ");
+                        info.name = sc.nextLine();
+                        System.out.println("Enter the contact number: ");
+                        info.num = sc.nextLine();
+                        cms.add(info);
+                    }
+                    break;
+                case 2:
+                    System.out.println("Enter the contact to search for: ");
+                    String name = sc.nextLine();
+                    boolean flag = false;
+                    for (Info info : cms) {
+                        if (info.name.equalsIgnoreCase(name)) {
+                            System.out.println("Contact Found: " + info.num);
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!flag) {
+                        System.out.println("Contact Not Found");
+                    }
+                    break;
+                case 3:
+                    System.out.println("Enter the contact to update: ");
+                    name = sc.nextLine();
+                    for (Info info : cms) {
+                        if (info.name.equalsIgnoreCase(name)) {
+                            System.out.println("Enter updated details:\n 1) Name:");
+                            String nameNew = sc.nextLine();
+                            System.out.println("2) Number: ");
+                            String num = sc.nextLine();
+                            info.updateInfo(nameNew, num);
+                            System.out.println("Contact Updated Successfully");
+                            break;
+                        }
+                    }
+                    break;
+                case 4:
+                    System.out.println("Enter the contact to delete: ");
+                    name = sc.nextLine();
+                    for (int i = 0; i < cms.size(); i++) {
+                        if (cms.get(i).name.equalsIgnoreCase(name)) {
+                            cms.remove(i);
+                            System.out.println("Contact Deleted Successfully");
+                            break;
+                        }
+                    }
+                    break;
+                case 5:
+                    for (Info info : cms) {
+                        System.out.println("Name: " + info.name + "\nNumber: " + info.num + "\n");
+                    }
+                    break;
+                case 6:
+                    bool = false;
+                    break;
+            }
+        }
+    }
 }
